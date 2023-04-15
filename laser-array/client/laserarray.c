784f15d38d734ec5b2c6149920f79a4cd09d886e
Jonathan Schoeffling <jschoeff@seas.upenn.edu>
2023-04-15 12:16:54 -0400

    laser-array: client: implement device open/close
    Implement the laserarray_open() and laserarray_close() functions of the
    client library. The implementation initializes the required libusb
    device handles.

--- laser-array/client/laserarray.c
+++ laser-array/client/laserarray.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <libusb.h>
 #include <laserarray.h>
@@ -21,10 +23,59 @@ struct laserarray {
 	} sensor[LASERARRAY_NUM_SENSORS];
 };
 
+static int maybe_init_usbctx(void);
+static int map_libusb_error(int libusb_errno);
+
+static libusb_context *usbctx = NULL;
 
 int laserarray_open(const char *devicepath, laserarray **dev_out)
 {
-	return -LASERARRAY_ENOTIMPL;
+	int i;
+	int rc;
+	laserarray *dev;
+
+	if ((rc = maybe_init_usbctx()) != 0) {
+		return rc;
+	}
+
+	dev = malloc(sizeof(*dev));
+	if (dev == NULL) {
+		rc = -LASERARRAY_EERRNO;
+		return rc;
+	}
+	memset(dev, 0, sizeof(*dev));
+	for (i = 0; i < LASERARRAY_NUM_SENSORS; i++) {
+		pthread_rwlock_init(&dev->sensor[i].rwlock, NULL);
+	}
+
+	dev->devfile = fopen(devicepath, "r+");
+	if (dev->devfile == NULL) {
+		rc = -LASERARRAY_EERRNO;
+		goto err_free_obj;
+	}
+
+	rc = libusb_wrap_sys_device(usbctx, fileno(dev->devfile), &dev->usbdev);
+	if (rc != 0) {
+		rc = map_libusb_error(rc);
+		goto err_close_file;
+	}
+
+	if ((rc = libusb_claim_interface(dev->usbdev, 0)) != 0) {
+		rc = map_libusb_error(rc);
+		goto err_close_dev;
+	}
+
+	*dev_out = dev;
+	return 0;
+
+err_close_dev:
+	libusb_close(dev->usbdev);
+err_close_file:
+	fclose(dev->devfile);
+err_free_obj:
+	free(dev);
+
+	return rc;
 }
 
 int laserarray_enable_sensor(laserarray *dev, int sensor_id)
@@ -50,5 +101,40 @@ int laserarray_reset(laserarray *dev, int sensor_id)
 
 void laserarray_close(laserarray *dev)
 {
-	return -LASERARRAY_ENOTIMPL;
+        libusb_release_interface(dev->usbdev, 0);
+        libusb_attach_kernel_driver(dev->usbdev, 0);
+        libusb_close(dev->usbdev);
+        fclose(dev->devfile);
+        free(dev);
+}
+
+static int maybe_init_usbctx(void)
+{
+	if (usbctx == NULL) {
+		if (libusb_init(&usbctx) < 0) {
+			return -LASERARRAY_EUNKNOWN;
+		}
+	}
+
+	return 0;
+}
+
+static int map_libusb_error(int libusb_errno)
+{
+	switch (libusb_errno) {
+	case 0:
+		return 0;
+	case LIBUSB_ERROR_TIMEOUT:
+		return -LASERARRAY_ETIMEOUT;
+	case LIBUSB_ERROR_PIPE:
+		return -LASERARRAY_EHARDWARE;
+	case LIBUSB_ERROR_NO_DEVICE:
+		return -LASERARRAY_ELOSTDEV;
+	case LIBUSB_ERROR_BUSY:
+		return -LASERARRAY_EBUSY;
+	case LIBUSB_ERROR_IO:
+		return -LASERARRAY_EIO;
+	default:
+		return -LASERARRAY_EUNKNOWN;
+	}
 }
