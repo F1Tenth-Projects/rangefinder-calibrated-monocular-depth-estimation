256b98d0e1ea97f5246bd7009bd88e8b8e2db7fa
Jonathan Schoeffling <jschoeff@seas.upenn.edu>
2023-05-04 12:37:43 -0400

    fusion: add (completely untested) fusion example code
    Add a notional implementation of the fuse_data() and main() functions,
    along with a stub for the ROS LaserScan publisher.

--- sensor-fusion/fusion.py
+++ sensor-fusion/fusion.py
@@ -2,7 +2,10 @@
 
 def main():
     while True:
-        ...
+        midas_map = get_midas_map()
+        sensor_data = get_sensor_data()
+        absolute_depth_map = fuse_data(midas_map, sensor_data)
+        publish_laserscan(absolute_depth_map)
 
 def fuse_data(midas_map, sensor_data):
     ''' fuse_data(midas_map, [[range, ...], ...]) -> cv2.Mat
@@ -11,7 +14,38 @@ def fuse_data(midas_map, sensor_data):
     sensor and fuses them into a depth map with absolute range values.
     Returns a cv2.Map object containing absolute depth measurements.
     '''
-    return None
+    img = []
+    img.append(midas_map[101:154, 0:49])
+    img.append(midas_map[101:154, 49:102])
+    img.append(midas_map[101:154, 102:155])
+    img.append(midas_map[101:154, 155:208])
+    img.append(midas_map[101:154, 208:256])
+
+    scale_factors = []
+    for i in range(0, len(img)):
+        scale = calc_scale_factor(img[i], sensor_data[i])
+        scale_factors.append(scale)
+
+    merged_scale = merge_scaling_factors(scale_factors)
+
+    return midas_map * merged_scale
+
+def calc_scale_factor(imageslice, rangelist):
+    ''' calc_scale_factor(imageslice, [range, ...]) -> float
+
+    Takes the segment 'imageslice' of a normalized depth map and
+    determines a floating point scaling factor to convert the
+    normalized distances to absolute distances.
+    '''
+    raise NotImplementedError()
+
+def merge_scale_factor(scale_list):
+    ''' merge_scale_factor([scale, ...]) -> float
+
+    Intelligently determine the global scaling factor for the depth
+    map based on the list of image segment scaling factors provided. 
+    '''
+    raise NotImplementedError()
 
 def get_midas_map():
     ''' get_midas_map() -> cv2.Mat
@@ -28,6 +62,14 @@ def get_sensor_data(device):
     the inner lists may contain between 0 and 8 range values.
     '''
 
+    raise NotImplementedError()
+
+def publish_laserscan(depth_map):
+    ''' publish_laserscan(depth_map):
+
+    Publishes the ROS LaserScan message using the scaled depth map data.
+    'depth_map' is a cv2.Mat object.
+    '''
     raise NotImplementedError()
 
 
